d7f0b0f3fb7b0392f6bac2e89a9bcddae8e56c21
TrieuXHuy <63645333+TrieuXHuy@users.noreply.github.com>
2026-05-07 08:34:22 +0700

    first commit

---
+++ src/test/java/com/salonnbooking/SalonnBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salonnbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonnBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
