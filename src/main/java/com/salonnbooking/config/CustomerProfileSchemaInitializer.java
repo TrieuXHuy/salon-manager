eceb3bb6e48021125292b65f7dc1fc7f4cdfdb9b
huytx <huytx@softdreams.vn>
2026-06-10 16:48:46 +0700

    oce

--- src/main/java/com/salonnbooking/config/CustomerProfileSchemaInitializer.java
+++ src/main/java/com/salonnbooking/config/CustomerProfileSchemaInitializer.java
@@ -5,7 +5,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-@Component
 public class CustomerProfileSchemaInitializer implements ApplicationRunner {
 	private final JdbcTemplate jdbcTemplate;
 
@@ -16,22 +15,50 @@ public class CustomerProfileSchemaInitializer implements ApplicationRunner {
 	@Override
 	public void run(ApplicationArguments args) {
 		jdbcTemplate.execute("""
-				IF COL_LENGTH('dbo.customers', 'username') IS NULL
-				    ALTER TABLE [dbo].[customers] ADD [username] varchar(50) NULL
-				""");
+        IF COL_LENGTH('dbo.customers', 'username') IS NULL
+            ALTER TABLE [dbo].[customers] ADD [username] varchar(50) NULL
+    """);
+
+		jdbcTemplate.execute("""
+        IF EXISTS (SELECT 1 FROM sys.indexes 
+                   WHERE name = N'UX_customers_phone'
+                   AND object_id = OBJECT_ID(N'dbo.customers'))
+            DROP INDEX [UX_customers_phone] ON [dbo].[customers]
+    """);
+
+		jdbcTemplate.execute("""
+        IF EXISTS (SELECT 1 FROM sys.indexes 
+                   WHERE name = N'UX_customers_phone_not_null'
+                   AND object_id = OBJECT_ID(N'dbo.customers'))
+            DROP INDEX [UX_customers_phone_not_null] ON [dbo].[customers]
+    """);
+
+		jdbcTemplate.execute("""
+        IF EXISTS (SELECT 1 FROM sys.indexes 
+                   WHERE name = N'UKm3iom37efaxd5eucmxjqqcbe9'
+                   AND object_id = OBJECT_ID(N'dbo.customers'))
+            DROP INDEX [UKm3iom37efaxd5eucmxjqqcbe9] ON [dbo].[customers]
+    """);
+
 		jdbcTemplate.execute("ALTER TABLE [dbo].[customers] ALTER COLUMN [full_name] nvarchar(255) NULL");
 		jdbcTemplate.execute("ALTER TABLE [dbo].[customers] ALTER COLUMN [phone] varchar(20) NULL");
+
 		jdbcTemplate.execute("""
-				IF EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'UX_customers_phone' AND object_id = OBJECT_ID(N'dbo.customers'))
-				    DROP INDEX [UX_customers_phone] ON [dbo].[customers]
-				""");
+        IF NOT EXISTS (SELECT 1 FROM sys.indexes 
+                       WHERE name = N'UX_customers_phone_not_null'
+                       AND object_id = OBJECT_ID(N'dbo.customers'))
+            CREATE UNIQUE INDEX [UX_customers_phone_not_null]
+            ON [dbo].[customers] ([phone])
+            WHERE [phone] IS NOT NULL
+    """);
+
 		jdbcTemplate.execute("""
-				IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'UX_customers_phone_not_null' AND object_id = OBJECT_ID(N'dbo.customers'))
-				    CREATE UNIQUE INDEX [UX_customers_phone_not_null] ON [dbo].[customers] ([phone]) WHERE [phone] IS NOT NULL
-				""");
-		jdbcTemplate.execute("""
-				IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'UX_customers_username_not_null' AND object_id = OBJECT_ID(N'dbo.customers'))
-				    CREATE UNIQUE INDEX [UX_customers_username_not_null] ON [dbo].[customers] ([username]) WHERE [username] IS NOT NULL
-				""");
+        IF NOT EXISTS (SELECT 1 FROM sys.indexes 
+                       WHERE name = N'UX_customers_username_not_null'
+                       AND object_id = OBJECT_ID(N'dbo.customers'))
+            CREATE UNIQUE INDEX [UX_customers_username_not_null]
+            ON [dbo].[customers] ([username])
+            WHERE [username] IS NOT NULL
+    """);
 	}
 }
